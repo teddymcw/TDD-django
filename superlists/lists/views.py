5bd6ea178c47ed62ac256aa2d4d36860651a9dee
teddymcw <teddymcw@gmail.com>
2015-11-07 13:59:03 -0800

    prob ch 4 just before pulling from repo

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
+
 def home_page(request):
-    # return HttpResponse('<html><title>To-Do lists</title></html>')    
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+            'new_item_text': request.POST.get('item_text', ''), 
+            # still should know exactly where this dict-like object lives 
+            # in the object that packs all the request data
+        })
